4283756182c9efe16d77d174d7567fca15aefea9
Saad Sayyed <saadsyed950@gmail.com>
2026-04-02 16:11:13 +0530

    1 - Two Sums (Bad Approach)

---
+++ arrays-hashing/1-two_sums/bad.py
@@ -0,0 +1,12 @@
+def badApproach(nums: list[int], target: int) -> list[int]:
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+    return []
+
+nums = [3, 4, 5 ,6]
+target = 7
+
+print(badApproach(nums, target))
