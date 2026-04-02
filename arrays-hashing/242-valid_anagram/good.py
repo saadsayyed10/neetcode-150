5c00653f0ceb00fa6433272802a9aac26bc3b1c5
Saad Sayyed <saadsyed950@gmail.com>
2026-04-02 14:26:36 +0530

    242 - Valid Anagram (Good Approach)

---
+++ arrays-hashing/242-valid_anagram/good.py
@@ -0,0 +1,20 @@
+def goodApproach(s: str, t: str) -> bool:
+    if(len(s) != len(t)):
+        return False
+    
+    countS = {}
+    countT = {}
+
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        countT[t[i]] = 1 + countT.get(t[i], 0)
+
+    return countS == countT
+
+s = "rat"
+t = "tar"
+
+p = "race"
+q = "sake"
+
+print("Is an anagram:", goodApproach(p, q))
